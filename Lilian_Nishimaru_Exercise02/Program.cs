4a3f24a3a93df1402742a074a52fabe3e485c6e2
lnishimaru <lilian.nishimaru@gmail.com>
2020-01-31 09:02:36 -0500

    Add project files.

---
+++ Lilian_Nishimaru_Exercise02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lilian_Nishimaru_Exercise02
+{
+    //delegate to produce a bool and receive a double if the parameter matches the lambda expression criteria
+    public delegate bool GradesPredicate(double grade);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] grades = new double[] { 10.5,100,20,55,80.2,95.8,34.7,89.0,67.5,90} ;
+            //call the method to filter grades greater or equal 50
+            GradesFilter(grades, x => (x >= 50));
+        }
+        //Method to filter the grades array with a lambda expression passed 
+        //input is grades array and  predicate (lambda expression) 
+        static void GradesFilter(double [] numbers, GradesPredicate test)
+        {
+            Console.WriteLine("Grades above or equal 50:");
+            foreach (double number in numbers)
+            {
+                if (test(number))
+                {
+                    Console.WriteLine($"{number}");
+                }
+            }
+        }
+    }
+}
---
+++ Lilian_Nishimaru_Sec003_COMP212_Lab01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lilian_Nishimaru_Sec003_COMP212_Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //func delegate to get the smallest of 3 strings using lambda expression  
+            Func<string, string, string, string> testFunc2 = delegate (string value1, string value2, string value3)
+            {
+                if (String.Compare(value1, value2, StringComparison.Ordinal) <= 0 &&
+                String.Compare(value1, value3, StringComparison.Ordinal) <= 0) //value1 is the smallest
+                {
+                     return value1;
+                }
+                else
+                {
+                    if (String.Compare(value2, value1, StringComparison.Ordinal) <= 0 &&
+                        String.Compare(value2, value3, StringComparison.Ordinal) <= 0) //value2 is the smallest
+                    {
+                        return value2;
+                    }
+                    else
+                    {
+                        if (String.Compare(value3, value1, StringComparison.Ordinal) <= 0 &&
+                            String.Compare(value3, value2, StringComparison.Ordinal) <= 0) //value3 is the smallest
+                        {
+                            return value3;
+                        }
+                        else //values are equal
+                        {
+                            return value1;
+                        }
+                    }
+
+                }
+            };
+            string lesser = testFunc2("Lion", "Elephant", "Zebra");
+            Console.WriteLine($"The smallest value is {lesser}");
+            
+            // action delegate to get the grades average
+            Action<double, double, double> testAction = AvgGrade;
+            testAction(7.5, 8, 9.5);
+
+        } // end main
+
+        //Method to calculate the average of 3 grades 
+        private static void AvgGrade(double grade1, double grade2, double grade3)
+        {
+            Console.WriteLine($"Average is {(grade1 + grade2 + grade3) / 3:F2}");
+        }
+    } // end class
+} // end namespace
